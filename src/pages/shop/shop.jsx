befc069aafd09398e18915703464aa28787dea12
MatiasBEZ <94873201+MatiasBEZ@users.noreply.github.com>
2023-01-31 23:17:21 -0300

    a filter by product category has been added

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import {PRODUCTS} from "../../products";
+import React, {useState} from "react";
+import {PRODUCTS} from "../../products.js";
 import {Product} from "./product";
 import "./shop.css";
 
 export const Shop = () => {
+    const [selectedCategory, setSelectedCategory] = useState("all");
+
+    function filterValueChange(e) {
+        setSelectedCategory(e.target.value);
+    }
+
     return (
         <div className="shop">
-            <div className="shopTitle">
+            <div className="shopFilter">
+                <label for="categories">Choose a category: </label>
+                <select id="categories" onChange={filterValueChange}>
+                    <option value="all">All</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="fashion">Fashion</option>
+                </select>
             </div>
-            <div className="products">
-                {PRODUCTS.map((product) => (
-                    <Product data={product}/>
-                ))}
+            <div className="products"> 
+                {selectedCategory === "all" 
+                    ? 
+                    PRODUCTS.map((product) => (<Product data={product}/>)) 
+                    : 
+                    PRODUCTS.filter(product => product.category === selectedCategory).map((product) => (
+                        <Product data={product}/>))
+                }
             </div>
         </div>
     );
