ce0610ac5a146688cabe4f1bec277c8db595f789
MatiasBEZ <94873201+MatiasBEZ@users.noreply.github.com>
2023-02-01 10:59:14 -0300

    filter functionality has been refactored and sortby price has been added

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -5,28 +5,57 @@ import "./shop.css";
 
 export const Shop = () => {
     const [selectedCategory, setSelectedCategory] = useState("all");
+    const [selectedSort, setSelectedSort] = useState("lowest");
 
-    function filterValueChange(e) {
+    const handlefilterChange = (e) => {
         setSelectedCategory(e.target.value);
     }
 
+    const handleSortChange = (e) => {
+        setSelectedSort(e.target.value);
+    }
+
+    const filteredProducts = PRODUCTS
+        .filter((product) => {
+            if (selectedCategory === "all") return true;
+            return product.category === selectedCategory;
+        })
+        .sort((a, b) => {
+            if (selectedSort === "lowest") {
+                if (a.price < b.price) return -1;
+                if (a.price > b.price) return 1;
+                return 0;
+            } else if (selectedSort === "highest") {
+                if (a.price > b.price) return -1;
+                if (a.price < b.price) return 1;
+                return 0;
+            }
+        })
+
     return (
         <div className="shop">
-            <div className="shopFilter">
-                <label for="categories">Choose a category: </label>
-                <select id="categories" onChange={filterValueChange}>
-                    <option value="all">All</option>
-                    <option value="electronics">Electronics</option>
-                    <option value="fashion">Fashion</option>
-                </select>
+            <div className="filters">
+                <div className="shopFilter">
+                    <label htmlFor="categories">Choose a category: </label>
+                    <select id="categories" onChange={handlefilterChange}>
+                        <option value="all">All</option>
+                        <option value="electronics">Electronics</option>
+                        <option value="fashion">Fashion</option>
+                    </select>
+                </div>
+                <div className="sortBy">
+                    <label htmlFor="sortOptions">SortBy: </label>
+                    <select id="sortOptions" onChange={handleSortChange}>
+                        <option value="lowest">Lowest Price</option>
+                        <option value="highest">Highest Price</option>
+                    </select>
+                </div>
             </div>
-            <div className="products"> 
-                {selectedCategory === "all" 
-                    ? 
-                    PRODUCTS.map((product) => (<Product data={product}/>)) 
-                    : 
-                    PRODUCTS.filter(product => product.category === selectedCategory).map((product) => (
-                        <Product data={product}/>))
+            <div className="products">
+                {
+                    filteredProducts.map((product) =>
+                    (<Product data={product} key={product.id}/>)
+                    )
                 }
             </div>
         </div>
