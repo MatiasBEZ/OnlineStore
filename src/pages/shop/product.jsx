4523edfc29dd607ff5b635aed1a22019606f6475
MatiasBEZ <94873201+MatiasBEZ@users.noreply.github.com>
2023-02-08 21:26:32 -0300

    remove from cart button has been added to shop page

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import {ShopContext} from "../../context/shop-context";
 import {ShoppingCart} from 'phosphor-react';
+import {Trash} from 'phosphor-react';
 
 export const Product = (props) => {
     const {id, productName, price, productImage } = props.data;
-    const {addToCart, cartItems} = useContext(ShopContext);
+    const {addToCart,removeFromCart, cartItems} = useContext(ShopContext);
     const cartItemAmount = cartItems[id]
 
     return <div className="product">
@@ -15,9 +16,18 @@ export const Product = (props) => {
             </p>
             <p> ${price} </p>
         </div>
-        <button className="addToCartBttn" onClick={() => addToCart(id)}>
-            <ShoppingCart size={20}/> 
-            Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
-        </button>
+        <div className="addOrRemoveBttns">
+            <button className="addTo CartBttn" onClick={() => addToCart(id)}>
+                <ShoppingCart size={20}/> 
+                Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+            </button>
+            {cartItemAmount > 0 
+            ?
+            <button className="RemoveFrom CartBttn" onClick={() => removeFromCart(id)}>
+                <Trash size={20} /> Remove </button>
+            :
+            null
+            }
+        </div>
     </div>;
 };
