7a4600852ed78280f980ebc29f3d8f32f757fafc
MatiasBEZ <94873201+MatiasBEZ@users.noreply.github.com>
2023-02-15 20:22:58 -0300

    a counter has been added to the cart nav icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import {Link} from "react-router-dom";
 import {ShoppingCart} from 'phosphor-react';
 import "./navbar.css";
 import HamburguerMenu from "./HamburguerMenu";
+import {ShopContext} from "../context/shop-context";
 
 export const Navbar = () => {
+    const {cartItems} = useContext(ShopContext);
+
+    const itemCount = Object.values(cartItems).reduce((acc, count) => acc + count, 0);
+
     return (
         <div className="navbar">
             <HamburguerMenu/>
@@ -14,6 +19,7 @@ export const Navbar = () => {
             <div className="links">
                 <Link to="/cart">
                     <ShoppingCart size={32}/>
+                    {itemCount > 0 ? <span className="cartCounter">{itemCount}</span> : null}
                 </Link>
             </div>
         </div>
