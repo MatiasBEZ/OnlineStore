c89f69c5f9f4172ad52c64e32f2bed0c542e1435
MatiasBEZ <94873201+MatiasBEZ@users.noreply.github.com>
2023-02-17 20:04:47 -0300

    I just added a simple about us page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -4,9 +4,19 @@ import "./about.css";
 export const About = () => {
 
     return (
-        <div className="cart">
-            <div>
-                <h1> Lorem blabla lmao oh yeah.</h1>
+        <div className="about-container">
+            <img src={process.env.PUBLIC_URL + `/Imgs/store.png`} />
+            <div className="about-content">
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus sollicitudin nisl 
+                    sodales feugiat. Donec euismod justo nec risus dapibus aliquam. Quisque eu quam a felis
+                    finibus dictum sed ac tellus. Duis sodales auctor eros, ut porta quam eleifend quis.
+                    Integer lobortis purus vel urna pharetra, in pulvinar orci ultrices.
+                    Donec accumsan sit amet diam in feugiat. Donec auctor diam eu nulla
+                    fringilla mollis non sit amet ipsum. Curabitur et porttitor lacus,
+                    at posuere nisi. Donec scelerisque nisi sapien, et pellentesque 
+                    urna vulputate sed. Vestibulum a faucibus erat.
+                </p>
             </div>
         </div>
     );
